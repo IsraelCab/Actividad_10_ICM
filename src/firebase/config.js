934978ceb7eb786e0208cf3ff44addf658badf6e
IsraelCab <israelcabmanz27@gmail.com>
2022-03-30 16:11:24 -0700

    Primer version Actividad 10 Israel Cabral TID41M

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4KaJ-MArSxzaByKuzyaM7QU1rBTRwt78',
-  authDomain: 'actividad-10-utch.firebaseapp.com',
-  databaseURL: 'https://actividad-10-utch-default-rtdb.firebaseio.com',
-  projectId: 'actividad-10-utch',
-  storageBucket: 'actividad-10-utch.appspot.com',
-  messagingSenderId: '912457574451',
-  appId: '1:912457574451:web:27ad1971316a81d0595b46',
+  apiKey: "AIzaSyB0SVuEDKUZjdXC-Ty_5kkXA8gL3hpWieU",
+  authDomain: "actividad-10-icm.firebaseapp.com",
+  databaseURL: "https://actividad-10-icm-default-rtdb.firebaseio.com",
+  projectId: "actividad-10-icm",
+  storageBucket: "actividad-10-icm.appspot.com",
+  messagingSenderId: "282665042064",
+  appId: "1:282665042064:web:7fa16bf44e0f36568a1815"
 };
 
 if (!firebase.apps.length) {
